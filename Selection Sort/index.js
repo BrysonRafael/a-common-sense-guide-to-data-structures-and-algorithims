6206a25460fae11589506bbc89598396c5073dea
BrysonRafael <bryson.rafael@gmail.com>
2019-04-23 16:34:00 -0500

    Added Jay Wengrow implementation of selection sort algorithim

--- Selection Sort/index.js
+++ Selection Sort/index.js
@@ -25,3 +25,27 @@ function selectionSort(arr) {
 
 const myArr = [50, 3, 34, 22, 70];
 console.log(selectionSort(myArr));
+
+/*  --Jay Wengrow implementation
+
+function selectionSort(array) {
+  for (var i = 0; i < array.length; i++) {
+    var lowestNumberIndex = i;
+
+    for (var j = i + 1; j < array.length; j++) {
+      if (array[j] < array[lowestNumberIndex]) {
+        lowestNumberIndex = j;
+      }
+    }
+
+    if (lowestNumberIndex !== i) {
+      var temp = array[i];
+      array[i] = array[lowestNumberIndex];
+      array[lowestNumberIndex] = temp;
+    }
+  }
+
+  return array;
+}
+
+*/
